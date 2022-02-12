3f5162f587e94685f23606f9699e3b25ce29f015
And-Salei <saleiandrew@gmail.com>
2022-02-12 14:39:06 +0200

    Update-app.js

--- js/app.js
+++ js/app.js
@@ -2,17 +2,17 @@ let IPN = '3261900854'
 IPN = IPN.split('').map (i => +i);
 let [a,b,c,d,e]=IPN;
 let arr = [a,b,c,d,e].join('');
-console.log(arr);
+console.log(+arr);
+
 
 let dFirst = new Date (1899, 11, 31, 12, 0, 0);
 console.log (+dFirst);
+let dFirst1 = +dFirst/60/60/1000/24/365;
+console.log (dFirst1);
 
 
-
-let dBD = [arr]*60*60*1000*24 + dFirst;
-console.log (dBD);
-
-
+let dCount = (arr-dFirst1)/365;
+console.log (dCount);
 
 
 
